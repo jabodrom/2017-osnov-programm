1340a2c7ae4223f81bebb5f69bf351a20ebf781d
jabodrom <jabodrom@gmail.com>
2017-11-01 18:29:44 +0100

    add tokeniser

--- segmenter.py
+++ segmenter.py
@@ -5,3 +5,5 @@ text = sys.stdin.read()
 text = text.replace('. ',  '.\n')
 text = text.replace('с.\nш.\n', 'с. ш. ' )
 text = text.replace('з.\nд.\n', 'з. д. ' )
+
+print(text)
---
+++ tokeniser.py
@@ -0,0 +1,35 @@
+import sys
+
+text = sys.stdin.readlines()
+
+sent_id = 1
+
+for line in text:
+	line = line.strip()
+	if line == '':
+		continue
+	print('# sent_id = ', sent_id)
+	print('# text = ', line)
+	line = line.replace(':',  ' :')
+	line = line.replace(',',  ' ,')
+	line = line.replace('.',  ' .')
+	line = line.replace('(',  '( ')
+	line = line.replace(')',  ' )')
+	line = line.replace('?',  ' ?')
+	line = line.replace('!',  ' !')
+	line = line.replace(';',  ' ;')
+	line = line.replace(' \'',  ' \' ')
+	line = line.replace('\' ',  ' \' ')
+	line = line.replace(' \"',  ' \" ')
+	line = line.replace('\" ',  ' \" ')
+	line = line.replace('с . ш . ',  'с.ш. ')
+	line = line.replace('з . д . ',  'з.д. ')
+	sent_id = sent_id + 1
+	
+	token_id = 1
+	tokens = line.split(' ')
+	for token in tokens:
+		if token == '':
+			continue
+		print(token_id, '\t', token, ' \t_\t_\t_\t_\t_\t_\t_\t_')
+		token_id = token_id + 1
